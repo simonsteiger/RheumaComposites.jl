18253f5979229aca618a6384880fead8d382d3d0
simonsteiger <simon.j.steiger@gmail.com>
2024-07-25 09:33:09 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.51

---
+++ src/RheumaResponse.jl
@@ -0,0 +1,5 @@
+module RheumaResponse
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RheumaResponse
+using Test
+
+@testset "RheumaResponse.jl" begin
+    # Write your tests here.
+end
