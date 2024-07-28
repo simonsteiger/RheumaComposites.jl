c32712987a379cc249881b5ecc2bcea83cbd2848
Simon Steiger <106233551+simonsteiger@users.noreply.github.com>
2024-07-28 18:23:20 +0200

    fix-weighting (#4)
    * fix weighting
    
    - previous solution broke when used inside a module, since the Dict would then be indexed with `RheumaComposites.DAS28ESR` instead of just `:DAS28ESR`
    
    * add docs to weight

--- src/utils/weight.jl
+++ src/utils/weight.jl
@@ -1,33 +1,27 @@
-# FIXME Should these Dicts be NamedTuples? Probably only a speed buff?
-weightfuns_das28esr = (
-    t28=(x, t28) -> sqrt(t28) * 0.56,
-    s28=(x, s28) -> sqrt(s28) * 0.28,
-    pga=(x, pga) -> pga * 0.014,
-    apr=(x, apr) -> log(apr) * 0.7,
+weights_das28esr = (
+    t28=t28 -> sqrt(t28) * 0.56,
+    s28=s28 -> sqrt(s28) * 0.28,
+    pga=pga -> pga * 0.014,
+    apr=apr -> log(apr) * 0.7,
 )
 
-weightfuns_das28crp = (
-    t28=(x, t28) -> sqrt(t28) * 0.56,
-    s28=(x, s28) -> sqrt(s28) * 0.28,
-    pga=(x, pga) -> pga * 0.014,
-    apr=(x, apr) -> log1p(apr) * 0.36,
-)
-
-weightfuns = (
-    DAS28ESR=weightfuns_das28esr,
-    DAS28CRP=weightfuns_das28crp,
+weights_das28crp = (
+    t28=t28 -> sqrt(t28) * 0.56,
+    s28=s28 -> sqrt(s28) * 0.28,
+    pga=pga -> pga * 0.014,
+    apr=apr -> log1p(apr) * 0.36,
 )
 
 """
-    weight(c::T, v::Symbol)
+    weight(x::T) where {T}
 
-Calculate the degree to which score `s` is made up of component `v`. The weighed components of `s` sum to 1.
+Weight a composite score's components according to its weighting scheme.
 
 # Example
 
-```julia-repl
-julia> weight(DAS28CRP(4, 5, 12, 44), :pga)
-> 0.168
+```jldoctest
+julia> DAS28CRP(t28=2, s28=2, pga=54u"mm", apr=19u"mg/L") |> weight
+(0.7919595949289333, 0.39597979746446665, 0.756, 1.0784636184794367)
 ```
 """
 weight(x::T) where {T} = weight(WeightingScheme(T), x)
@@ -36,12 +30,23 @@ weight(::IsUnweightable, x::T) where {T} = throw(ErrorException("$(typeof(x)) ty
 
 weight(::IsUnweighted, x::T) where {T} = ustrip.(getproperty.(Ref(x), fieldnames(T)))
 
-function weight(::IsWeighted, x::T) where {T}
-    weightfuns_t = getproperty(weightfuns, Symbol(T))
-    weighted_values = map(fieldnames(T)) do component
+function map_weights(weights, x)
+    weighted_values = map(fieldnames(typeof(x))) do component
         component_value = ustrip(getproperty(x, component))
-        weight_t = getproperty(weightfuns_t, component)
-        weight_t(x, component_value)
+        component_weight = getproperty(weights, component)
+        component_weight(component_value)
     end
+    return weighted_values
+end
+
+function weight(::IsWeighted, x::DAS28CRP)
+    weights = weights_das28crp
+    weighted_values = map_weights(weights, x)
+    return weighted_values
+end
+
+function weight(::IsWeighted, x::DAS28ESR)
+    weights = weights_das28esr
+    weighted_values = map_weights(weights, x)
     return weighted_values
 end
